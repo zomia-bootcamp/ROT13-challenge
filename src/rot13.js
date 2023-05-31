ab26f93e95b7865be93b1b479a8c6fd3fcbdf835
zomia-bootcamp <132401198+zomia-bootcamp@users.noreply.github.com>
2023-05-31 20:12:55 +0300

    Update rot13.js

--- src/rot13.js
+++ src/rot13.js
@@ -1,5 +1,50 @@
 "use strict";
 
-const rot13 = {};
+const rot13 = {
+  alphabet: 'abcdefghijklmnopqrstuvwxyz'.split(''),
+  rotatedAlphabet: 'nopqrstuvwxyzabcdefghijklm'.split(''),
+
+  _encodeLetter: function (letter) {
+    const index = this.alphabet.indexOf(letter)
+    const encodedLetter = this.rotatedAlphabet[index]
+
+    return encodedLetter
+  },
+
+  _decodeLetter: function (letter) {
+    const index = this.rotatedAlphabet.indexOf(letter)
+    const decodedLetter = this.alphabet[index]
+
+    return decodedLetter
+  },
+
+  encode: function (message) {
+    const words = message.split(' ')
+
+    const encodeWord = word => {
+      return word.split('').map(letter => {
+        return this._encodeLetter(letter)
+      }).join('')
+    }
+
+    const encodedWords = words.map(encodeWord)
+
+    return encodedWords.join(' ')
+  },
+
+  decode: function (message) {
+    const words = message.split(' ')
+
+    const decodeWord = word => {
+      return word.split('').map(letter => {
+        return this._decodeLetter(letter)
+      }).join('')
+    }
+
+    const decodedWords = words.map(decodeWord)
+
+    return decodedWords.join(' ')
+  }
+};
 
 module.exports = rot13;
